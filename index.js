5b821410f5d91390bf6e4bb104e1854066a9d3d6
elvin <elvin.ogingo@student.moringaschool.com>
2026-09-25 20:39:59 +0300

    Completed lab functions

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
+
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  const reversed = word.split('').reverse().join('');
+  return word === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * (discountPercentage / 100));
+}
+
+// Export the functions for testing
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice,
+};
 
 
 
